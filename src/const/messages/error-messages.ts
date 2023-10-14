1dec3225357c194dafe8e8731fb8d6d68ffb9680
Ihor Sharov <cookie50ds@gmail.com>
2023-10-14 17:08:35 +0300

    add authentication guard

--- src/const/messages/error-messages.ts
+++ src/const/messages/error-messages.ts
@@ -1,3 +1,4 @@
 export const SOMETHING_WENT_WRONG = 'Something went wrong';
 export const INCORRECT_USER_CREDENTIALS = 'Incorrect email or password';
 export const USER_EXIST = 'The user already exists';
+export const MISSING_TOKEN = 'This action requires authentication';
---
+++ src/modules/auth/auth.guard.ts
@@ -0,0 +1,39 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import * as messages from '../../const/messages';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException(messages.MISSING_TOKEN);
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      request['user'] = payload;
+    } catch (e) {
+      throw new UnauthorizedException(
+        e.message || messages.SOMETHING_WENT_WRONG,
+      );
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
